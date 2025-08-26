b2c69c691c94730806bf00a77435f68820e17e47
mengjin67 <jin.meng.uiowa@gmail.com>
2025-08-26 13:09:17 -0500

    Add data ETL code to the pipeline

---
+++ analysis_tool_chest/data_etl.py
@@ -0,0 +1,39 @@
+class DataETL:
+    """Class to perform ETL (Extract, Transform, Load) operations on the Auto Businessline DataFrame."""
+    import pandas as pd
+    import os
+    from ydata_profiling import ProfileReport
+
+    def __init__(self, data):
+        self.data = data
+
+    def profile_analysis(self, output_folder, file_name, title="Data Profiling Report"):
+        """Generate a profiling report for the current data and save it as an HTML file."""
+        self.os.makedirs(output_folder, exist_ok=True)
+        profile = self.ProfileReport(self.data, title=title, minimal=True)
+        profile.to_file(f"{output_folder}/{file_name}")
+
+    def transform(self):
+        """Perform ETL transformations on the data."""
+        # Convert 'date_of_birth' to datetime, coercing errors to NaT for invalid formats
+        self.data['date_of_birth'] = self.pd.to_datetime(self.data['date_of_birth'], format='%m/%d/%Y', errors='coerce')
+
+        # Define bins and labels for age categories
+        bins = [
+            self.pd.Timestamp('1900-01-01'), 
+            self.pd.Timestamp('1950-01-01'), 
+            self.pd.Timestamp('1960-01-01'),
+            self.pd.Timestamp('1970-01-01'), 
+            self.pd.Timestamp('1980-01-01'), 
+            self.pd.Timestamp('1990-01-01'), 
+            self.pd.Timestamp.max 
+        ]
+        labels = [1, 2, 3, 4, 5, 6]
+        # Use pd.cut to assign agecat2 based on date_of_birth
+        self.data['agecat2'] = self.pd.cut(self.data['date_of_birth'], bins=bins, labels=labels, right=False)
+        # Ensure float type for consistency
+        self.data['agecat2'] = self.data['agecat2'].astype(float)
+        # Group 'MCARA', 'CONVT', 'BUS', and 'RDSTR' 'veh_body' as 'Other'
+        self.data.loc[self.data['veh_body'].isin(['MCARA','CONVT','BUS','RDSTR']), 'veh_body'] = 'Other'
+
+        return self.data
