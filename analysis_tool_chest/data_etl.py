edf55243a9637934c11a2c1de33738d3700615b3
mengjin67 <jin.meng.uiowa@gmail.com>
2025-09-17 11:43:45 -0500

    Change data to 2023 UMN Travelers Analytics Case Competition and rerun the entire study

--- analysis_tool_chest/data_etl.py
+++ analysis_tool_chest/data_etl.py
@@ -1,5 +1,6 @@
 class DataETL:
     """Class to perform ETL (Extract, Transform, Load) operations on the Auto Businessline DataFrame."""
+
     import pandas as pd
     import os
     from ydata_profiling import ProfileReport
@@ -15,26 +16,6 @@ class DataETL:
 
     def transform(self, cap_dict=None):
         """Perform ETL transformations on the data. Optionally cap variables using a dictionary of {var: cap_value}."""
-        # Convert 'date_of_birth' to datetime, coercing errors to NaT for invalid formats
-        self.data['date_of_birth'] = self.pd.to_datetime(self.data['date_of_birth'], format='%m/%d/%Y', errors='coerce')
-
-        # Define bins and labels for age categories
-        bins = [
-            self.pd.Timestamp('1900-01-01'), 
-            self.pd.Timestamp('1950-01-01'), 
-            self.pd.Timestamp('1960-01-01'),
-            self.pd.Timestamp('1970-01-01'), 
-            self.pd.Timestamp('1980-01-01'), 
-            self.pd.Timestamp('1990-01-01'), 
-            self.pd.Timestamp.max 
-        ]
-        labels = [1, 2, 3, 4, 5, 6]
-        # Use pd.cut to assign agecat2 based on date_of_birth
-        self.data['agecat2'] = self.pd.cut(self.data['date_of_birth'], bins=bins, labels=labels, right=False)
-        # Ensure float type for consistency
-        self.data['agecat2'] = self.data['agecat2'].astype(float)
-        # Group 'MCARA', 'CONVT', 'BUS', and 'RDSTR' 'veh_body' as 'Other'
-        self.data.loc[self.data['veh_body'].isin(['MCARA','CONVT','BUS','RDSTR']), 'veh_body'] = 'Other'
 
         # Cap variables if cap_dict is provided
         if cap_dict is not None:
@@ -43,10 +24,15 @@ class DataETL:
                     self.data[var] = self.data[var].clip(upper=cap_value)
                     print(f"{var} capped at {cap_value}")
 
-        # Assume single vehicle policy and create a vehicle count variable
-        self.data['veh_cnt'] = 1
+        # Group 'MCARA', 'CONVT', 'BUS', and 'RDSTR' 'veh_body' as 'Other'
+        self.data.loc[
+            self.data["veh_body"].isin(["MCARA", "CONVT", "BUS", "RDSTR"]), "veh_body"
+        ] = "Other"
 
-        # Add policy year 
-        self.data['pol_year'] = 2018
+        # Assume single vehicle policy and create a vehicle count variable
+        self.data["veh_cnt"] = 1
+
+        # Add policy year
+        self.data["data_segment"] = "2|inference"
 
         return self.data
